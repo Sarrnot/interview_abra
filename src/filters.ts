68b85f89ecd4aa1db4ed2528759636282c271404
Sarrnot <urassus@gmail.com>
2024-03-03 17:46:24 +0100

    add algorithm for finding filter nodes in postal codes tree

--- src/filters.ts
+++ src/filters.ts
@@ -1,16 +1,6 @@
 import TreeNode from "./DataStructure/TreeNode";
 import Customer from "./types/Customer";
 
-export const createFilters = (customers: Customer[], filterCount: number) => {
-    const postalCodes = customers.map((customer) =>
-        customer.psc.replace(/ /g, "")
-    );
-
-    const tree = createTree(postalCodes);
-
-    return [];
-};
-
 const createTree = (values: string[]) => {
     const root = new TreeNode("");
 
@@ -19,10 +9,10 @@ const createTree = (values: string[]) => {
 
         currentNode.entriesCount += 1;
 
-        for (let i = 0; i < value.length; i++) {
-            const char = value[i];
+        for (let i = 1; i <= value.length; i++) {
+            const part = value.slice(0, i);
 
-            const nextNode = currentNode.addChild(char);
+            const nextNode = currentNode.addChild(part);
 
             currentNode.childrenEntriesCount += 1;
             nextNode.entriesCount += 1;
@@ -32,4 +22,55 @@ const createTree = (values: string[]) => {
     });
 
     return root;
+};
+
+const findFilterNodes = (root: TreeNode<string>, filterCount: number) => {
+    const splittableNodes: TreeNode<string>[] = [root];
+    const unsplittableNodes: TreeNode<string>[] = [];
+    const nodesCount = () => splittableNodes.length + unsplittableNodes.length;
+
+    while (splittableNodes.length > 0) {
+        if (nodesCount() === filterCount) break;
+
+        splittableNodes.sort((a, b) => {
+            if (a.entriesCount > b.entriesCount) {
+                return -1;
+            }
+            if (a.entriesCount < b.entriesCount) {
+                return 1;
+            }
+            return 0;
+        });
+
+        for (let i = 0; i < splittableNodes.length; i++) {
+            const node = splittableNodes[i];
+
+            const hasDirectEntries =
+                node.entriesCount > node.childrenEntriesCount;
+            const wouldExceedLimit =
+                nodesCount() + node.children.length - 1 > filterCount;
+
+            if (hasDirectEntries || wouldExceedLimit) {
+                unsplittableNodes.push(node);
+                splittableNodes.splice(i, 1);
+                continue;
+            }
+
+            splittableNodes.splice(i, 1, ...node.children);
+            break;
+        }
+    }
+
+    return [...splittableNodes, ...unsplittableNodes];
+};
+
+export const createFilters = (customers: Customer[], filterCount: number) => {
+    const postalCodes = customers.map((customer) =>
+        customer.psc.replace(/ /g, "")
+    );
+    const root = createTree(postalCodes);
+    const filterNodes = findFilterNodes(root, filterCount);
+    const filters: string[] = filterNodes.map((node) => node.value);
+
+    return filters;
 };
